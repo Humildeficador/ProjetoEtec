5cafa134a748941ececbbda6fb8027224f1a9e86
João Leandro <130767630+Humildeficador@users.noreply.github.com>
2023-05-25 21:36:36 -0300

    Update db.sql

--- db.sql
+++ db.sql
@@ -4,14 +4,30 @@ USE mercado;
 
 CREATE TABLE produto (
 	codProd INT NOT NULL,
-	descricao TEXT NOT NULL,
-	valor FLOAT NOT NULL,
-	vencimento DATE NOT NULL,
-)
+	descricao VARCHAR(30) NOT NULL,
+	valor DECIMAL(7,2) NOT NULL,
+	vencimento DATE NOT NULL
+);
 
 CREATE TABLE movimento (
 	codProd INT NOT NULL,
-	tipo VARCHAR(1) NOT NULL,
+	tipo CHAR(1) NOT NULL,
 	qnt INT NOT NULL,
 	dataMov TIMESTAMP  NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE cliente (
+	codCliente INT NOT NULL,
+	nome VARCHAR (60) NOT NULL,
+    cpf INT (11) NOT NULL,
+    celular INT (12) NOT NULL,
+    cep INT (8) NOT NULL
+);
+
+CREATE TABLE fornecedor (
+	codFornecedor INT NOT NULL,
+    nome VARCHAR (60) NOT NULL,
+    cnpj INT (11) NOT NULL,
+    celular INT (12) NOT NULL,
+    cep INT (8) NOT NULL
 );
