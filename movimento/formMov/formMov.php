36f9f77b8dd5d605ffc62bfd0982bf6bf3527840
João <joao.oliveira1270@etec.sp.gov.br>
2023-06-01 20:31:55 -0300

    add cod formMov

--- movimento/formMov/formMov.php
+++ movimento/formMov/formMov.php
@@ -16,35 +16,40 @@
 				CADASTRO
 			</header>
 			<form class="content" action="../back-end/cadastroMov.php" method="GET">
-				<div class="input-group">
-					<div class="input-box">
-						<label for="codProd">Código do produto</label>
-						<input type="number" name="codProd" min="1" max="9999999" required>
-					</div>
-					<div class="input-box">
-						<label for="qnt">Quantidade</label>
-						<input type="number" name="qnt" min="1" max="9999999" required>
-					</div>
-				</div>
-
-				<div class="radio-date">
-					<div class="radio-group">
-						<div class="radio-box">
-							<input type="radio" name="tipo" id="e" value="E">
-							<label for="e">Entrada</label>
+				<div class="box-box">
+					<div class="input-group">
+						<div class="input-box">
+							<label for="codProd">Código do produto</label>
+							<input type="number" name="codProd" min="1" max="9999999" required>
 						</div>
-						<div class="radio-box">
-							<input type="radio" name="tipo" id="s" value="S">
-							<label for="s">Saída</label>
+						<div class="input-box">
+							<label for="qnt">Quantidade</label>
+							<input type="number" name="qnt" min="1" max="9999999" required>
+						</div>
+						<div class="input-box">
+							<label for="qnt">Código</label>
+							<input type="number" name="cod" min="1" max="9999999" required>
 						</div>
 					</div>
-					<div class="date">
-						<script lang="javascript">
-							now = new Date;
-							document.write (`
-							Hoje é dia: ${now.getDate()}/${(now.getMonth()<10?'0':'') + now.getMonth()}/${now.getFullYear()}<br>
-							Hora Atual: ${now.getHours()}:${(now.getMinutes()<10?'0':'') +now.getMinutes()}`)
-						</script>
+					<div class="radio-date">
+						<div class="radio-group">
+							<div class="radio-box">
+								<input type="radio" name="tipo" id="e" value="E">
+								<label for="e">Entrada</label>
+							</div>
+							<div class="radio-box">
+								<input type="radio" name="tipo" id="s" value="S">
+								<label for="s">Saída</label>
+							</div>
+						</div>
+						<div class="date">
+							<script lang="javascript">
+								now = new Date;
+								document.write (`
+								Hoje é dia: ${(now.getDate()<10?'0':'') + now.getDate()}/${(now.getMonth()<10?'0':'') + now.getMonth()}/${now.getFullYear()}<br>
+								Hora Atual: ${now.getHours()}:${(now.getMinutes()<10?'0':'') +now.getMinutes()}`)
+							</script>
+						</div>
 					</div>
 				</div>
 
